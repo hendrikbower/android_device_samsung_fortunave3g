14a2f00f7458066482e84e5f9029aa2a6029cef0
hendrikbower <hendrik.1810@gmail.com>
2016-12-05 07:00:27 +0700

    Add files via upload

---
+++ android_device_samsung_fortunave3g/cm.mk
@@ -0,0 +1,17 @@
+# Release name
+PRODUCT_RELEASE_NAME := SM-G530H
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/fortunave3g/device_fortunave3g.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+
+PRODUCT_DEVICE := fortunave3g
+PRODUCT_NAME := cm_fortunave3g
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := SM-G530H
